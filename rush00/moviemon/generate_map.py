c88642efa6821f436afc1cc3d6e59d9b97065bfd
Adrien Milis <amilis@fu-r3-p5.s19.be>
2021-06-19 19:07:58 +0200

    generate_map with css

--- rush00/moviemon/generate_map.py
+++ rush00/moviemon/generate_map.py
@@ -1,8 +1,10 @@
 def write_columns(up_map, columns, pos_y):
 	for i in range(columns):
-		up_map.write('\t<td>\n')
+		up_map.write('\t<td back>\n')
 		if (pos_y == i):
-			up_map.write('\t\t<img src="./images/sacha.png" height=10 width=10>')
+			up_map.write("""\t\t<img src="{% static 'sacha.jpg' %}" height=30 width=30 style="text-align: center; vertical-align: middle;">\n""")
+		else:
+			up_map.write("""\t\t<img src="{% static 'grass.jpg' %}" height=30 width=30>\n""")
 		up_map.write('\t</td>\n')
 
 def generate_map(player_pos, rows, columns):
@@ -12,12 +14,12 @@ def generate_map(player_pos, rows, columns):
 	up_map.write('{% block title %}\n')
 	up_map.write('Map\n')
 	up_map.write('{% endblock %}\n')
-	up_map.write('{% load static %}')
-	up_map.write('{% block styles %}')
-	# up_map.write('') <link> to style.css
-	up_map.write('{% endblock %}\n')
+	up_map.write('{% load static %}\n')
+	up_map.write('{% block style %}\n')
+	up_map.write("""<link rel="stylesheet" type="text/css" href="{% static 'map_style.css' %}">\n""")
+	up_map.write('{% endblock %}')
 	up_map.write('{% block content %}\n')
-	up_map.write('<table>\n')
+	up_map.write("""<table>\n""")
 
 	for i in range(rows):
 		up_map.write('<tr>\n')
@@ -31,4 +33,4 @@ def generate_map(player_pos, rows, columns):
 	up_map.write('{% endblock %}\n')
 
 if __name__ == '__main__':
-	generate_map([0, 1], 5, 5)
+	generate_map([5, 5], 20, 20)
