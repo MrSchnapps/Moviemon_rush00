5e273215e5040ce52cd6c0aad0fdfb158ce0de92
Adrien Milis <amilis@fu-r3-p5.s19.be>
2021-06-19 16:36:47 +0200

    moviemon class and scraper

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -1,5 +1,14 @@
 import requests, json, sys
 
+class Moviemon():
+	def __init__(self, scraped_info):
+		self.title = scraped_info['title']
+		self.director = scraped_info['director']
+		self.year = scraped_info['year']
+		self.rating = scraped_info['rating']
+		self.actors = scraped_info['actors']
+		self.synopsis = scraped_info['synopsis']
+
 def scrape_imdb(movie_name):
 	url = 'http://www.omdbapi.com/?t={}&apikey=f72e10ff'.format(movie_name)
 
@@ -16,3 +25,6 @@ def scrape_imdb(movie_name):
 	scrape_dic['synopsis'] = data['Plot']
 
 	return(scrape_dic)
+
+Moviemon1-title:title-director:director-
+Moviemon2-
