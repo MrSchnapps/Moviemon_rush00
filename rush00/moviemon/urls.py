8f578d924e212b603b35f3f9e4723d680198c2a9
Adrien Milis <amilis@fu-r3-p5.s19.be>
2021-06-19 17:48:08 +0200

    working on map

--- rush00/moviemon/urls.py
+++ rush00/moviemon/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import MapPageView
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('worldmap/', MapPageView.as_view(), name='map')
 ]
