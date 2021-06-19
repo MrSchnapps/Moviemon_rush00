d4d751488e70539a0a2df1ee1e23af1fd4fa388a
Jules Decuypere <judecuyp@fu-r3-p7.s19.be>
2021-06-19 17:35:47 +0200

    settings

--- rush00/rush00/settings.py
+++ rush00/rush00/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Moviemon informations
+MAP_SIZE = 40
+STARTING_POINT = [29, 29]
