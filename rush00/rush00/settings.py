d103fdd31a9ea6da3a8eace18043dee10a1294af
Adrien Milis <amilis@fu-r3-p5.s19.be>
2021-06-19 17:40:44 +0200

    static path to settings.py

--- rush00/rush00/settings.py
+++ rush00/rush00/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_PATH = str(BASE_DIR.joinpath('media'))
 
+STATICFILES_DIRS = [
+    str(BASE_DIR.joinpath('static'))
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
