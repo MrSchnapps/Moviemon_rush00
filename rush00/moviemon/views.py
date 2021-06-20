3f16d2f0f27bd70865f3a04c49a0a657e52c8d8f
Adrien Milis <amilis@fu-r3-p5.s19.be>
2021-06-20 15:40:51 +0200

    views

--- rush00/moviemon/views.py
+++ rush00/moviemon/views.py
@@ -3,11 +3,12 @@ from random import *
 from django.conf import settings
 from .Game import *
 from django.views.generic import TemplateView
-from . import generate_map as g_map
-from . import imdb_scraper as scr
 from time import sleep
 from django.http import HttpResponseRedirect
 from .moviedex import *
+from . import generate_map as g_map
+from . import imdb_scraper as scr
+from battle import *
 # Create your views here.
 
 def loadMap() :
@@ -21,6 +22,9 @@ def loadMap() :
     # pos = g_map.get_pos(file_content)
     g_map.generate_map(game.pos[0], game.pos[1], settings.ROWS, settings.COLUMNS, 0, game)
     game.write_infos()
+    list_of_moviemons = scr.load_all_moviemons()
+    for moviemon in list_of_moviemons:
+
     # with open("moviemon/save1_example", 'r') as file :
     #     infos = file.read()
     # game.load(infos)
@@ -28,10 +32,12 @@ def loadMap() :
 class MapPageView(TemplateView):
 	template_name = 'map.html'
 
+### main page
 def index(request):
     loadMap()
     return render(request, 'index.html')
 
+### movement
 def moveup(request):
     g_map.move_character('up')
     return HttpResponseRedirect('/worldmap/')
@@ -48,6 +54,13 @@ def moveright(request):
     g_map.move_character('right')
     return HttpResponseRedirect('/worldmap/')
 
+### moviedex
 def moviedex(request) :
     generate_moviedex()
     return (render, 'moviedex.html')
+
+### battle
+def gladiator(request):
+    
+
+def throw_ball(request):
